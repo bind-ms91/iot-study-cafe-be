686d9d3d14906e7af4f4bfbe5037d6ee8dc49111
ms-cho91 <thirdxero@jj.ac.kr>
2024-07-16 18:05:01 +0900

    KAN-28 데이터 수정

--- src/main/java/bind/iotstudycafe/commons/exampleDomain/controller/ExampleDomainController.java
+++ src/main/java/bind/iotstudycafe/commons/exampleDomain/controller/ExampleDomainController.java
@@ -2,16 +2,21 @@ package bind.iotstudycafe.commons.exampleDomain.controller;
 
 
 import bind.iotstudycafe.commons.exampleDomain.domain.ExampleDomain;
+import bind.iotstudycafe.commons.exampleDomain.dto.ExampleDomainSave;
 import bind.iotstudycafe.commons.exampleDomain.dto.ExampleDomainSearchCond;
+import bind.iotstudycafe.commons.exampleDomain.dto.ExampleDomainUpdate;
 import bind.iotstudycafe.commons.exampleDomain.service.ExampleDomainService;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -24,9 +29,15 @@ public class ExampleDomainController {
     private ExampleDomainService exampleDomainService;
 
     @PostMapping("/save")
-    public ExampleDomain save(@RequestBody ExampleDomain exampleDomain) {
+    public ExampleDomain save(@Validated @RequestBody ExampleDomainSave exampleDomainSave) {
 
-        log.info("ExampleDomainController.save post exampleDomain: {}", exampleDomain);
+        log.info("ExampleDomainController.save post exampleDomainSave: {}", exampleDomainSave);
+
+        ExampleDomain exampleDomain = new ExampleDomain();
+        exampleDomain.setLoginId(exampleDomainSave.getLoginId());
+        exampleDomain.setPassword(exampleDomainSave.getPassword());
+        exampleDomain.setName(exampleDomainSave.getName());
+        exampleDomain.setAge(exampleDomainSave.getAge());
 
         return exampleDomainService.save(exampleDomain);
     }
@@ -39,28 +50,35 @@ public class ExampleDomainController {
         return exampleDomainService.findById(id);
     }
 
-//    @GetMapping("/list")
-//    public List<ExampleDomain> findExampleDomains(
-//            @RequestParam("name") String name,
-//            @RequestParam("age") Integer age) {
-//
-//        ExampleDomain exampleDomain = new ExampleDomain();
-//        exampleDomain.setName(name);
-//        exampleDomain.setAge(age);
-//
-//        return exampleDomainService.findExampleDomains(exampleDomain);
-//    }
 
     @GetMapping("/list")
-    public List<ExampleDomain> findExampleDomains(@Validated @ModelAttribute ExampleDomainSearchCond exampleDomainSearchCond) {
-
-        log.info("ExampleDomainController.findExampleDomains search cond: {}", exampleDomainSearchCond);
+    public List<ExampleDomain> findExampleDomains(
+            @RequestParam("name") @NotBlank String name,
+            @RequestParam(name = "age", required = false)  Integer age) {
 
         ExampleDomain exampleDomain = new ExampleDomain();
-        exampleDomain.setName(exampleDomainSearchCond.getName());
-        exampleDomain.setAge(exampleDomainSearchCond.getAge());
+        exampleDomain.setName(name);
+        exampleDomain.setAge(age);
 
         return exampleDomainService.findExampleDomains(exampleDomain);
     }
 
+//    @GetMapping("/list")
+//    public List<ExampleDomain> findExampleDomains(@Validated @ModelAttribute ExampleDomainSearchCond exampleDomainSearchCond) {
+//
+//        log.info("ExampleDomainController.findExampleDomains search cond: {}", exampleDomainSearchCond);
+//
+//        ExampleDomain exampleDomain = new ExampleDomain();
+//        exampleDomain.setName(exampleDomainSearchCond.getName());
+//        exampleDomain.setAge(exampleDomainSearchCond.getAge());
+//
+//        return exampleDomainService.findExampleDomains(exampleDomain);
+//    }
+
+    @PutMapping("/update/{id}")
+    public void update(@PathVariable Long id
+                        ,@Validated @RequestBody ExampleDomainUpdate updateParam) {
+
+        exampleDomainService.update(id, updateParam);
+    }
 }
--- src/test/java/bind/iotstudycafe/commons/exampleDomain/service/ExampleDomainServiceTest.java
+++ src/test/java/bind/iotstudycafe/commons/exampleDomain/service/ExampleDomainServiceTest.java
@@ -1,8 +1,13 @@
 package bind.iotstudycafe.commons.exampleDomain.service;
 
 import bind.iotstudycafe.commons.exampleDomain.domain.ExampleDomain;
+import bind.iotstudycafe.commons.exampleDomain.dto.ExampleDomainUpdate;
+import bind.iotstudycafe.commons.exampleDomain.repository.ExampleDomainRepository;
+import bind.iotstudycafe.commons.exampleDomain.repository.memory.MemoryExampleDomainRepository;
+import bind.iotstudycafe.member.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,23 +16,59 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 class ExampleDomainServiceTest {
 
-    @Autowired
-    private ExampleDomainService exampleDomainService;
+    ExampleDomainService exampleDomainService;
+
+    @BeforeEach
+    public void beforeEach() {
+        ExampleDomainRepository exampleDomainRepository = new MemoryExampleDomainRepository();
+        exampleDomainService = new ExampleDomainServiceImpl(exampleDomainRepository);
+    }
+
 
     @Test
     void findByIdToEntity() {
 
+        //given
         ExampleDomain exampleDomain = new ExampleDomain("ms90", "1111", "Chominseong", 20);
-
         exampleDomainService.save(exampleDomain);
 
+        //when
         ExampleDomain findExampleDomain = exampleDomainService.findById(exampleDomain.getId()).get();
 
         log.info("exampleDomain: {}", exampleDomain);
         log.info("findExampleDomain: {}", findExampleDomain);
 
+        //then
         Assertions.assertThat(exampleDomain).isEqualTo(findExampleDomain);
 
     }
+
+    @Test
+    void update() {
+
+        //given
+        ExampleDomain exampleDomain = new ExampleDomain("ms90", "1111", "Chominseong", 20);
+
+        exampleDomainService.save(exampleDomain);
+
+        Long id = exampleDomain.getId();
+
+
+        //when
+        ExampleDomainUpdate updateParam = new ExampleDomainUpdate("2222", "minseong", 22);
+        exampleDomainService.update(id, updateParam);
+
+        log.info("exampleDomain: {}", exampleDomain);
+
+        log.info("updateParam: {}", updateParam);
+
+        //then
+
+        Assertions.assertThat(exampleDomain.getPassword()).isEqualTo(updateParam.getPassword());
+        Assertions.assertThat(exampleDomain.getName()).isEqualTo(updateParam.getName());
+        Assertions.assertThat(exampleDomain.getAge()).isEqualTo(updateParam.getAge());
+
+    }
+
 
 }
